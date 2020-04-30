f33b211dbc1f08d7b58ddcc78e5201976a03216f
Jan Clemens <clemensjan@googlemail.com>
2020-04-30 13:04:49 +0200

    pipify

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 from setuptools import setup, find_packages
 import os
+import codecs
+import re
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +27,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='glm_utils',
-      version='0.1',
+      version=find_version("src/glm_utils/__init__.py"),
       description='glm_utils',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -17,7 +37,7 @@ setup(name='glm_utils',
       license='MIT',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      install_requires=['scikit-learn', 'numpy'],
+      install_requires=['numpy', 'scipy', 'scikit-learn'],
       include_package_data=True,
       zip_safe=False
       )
